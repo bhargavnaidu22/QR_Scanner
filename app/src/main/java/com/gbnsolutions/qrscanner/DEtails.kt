80a29f71e6215a3b24398e3fada79f07ca6ad8e3
bhargavnaidu22 <bhargavnaidu.gedela2002@gmail.com>
2022-02-22 14:27:36 +0530

    First commit

---
+++ app/src/main/java/com/gbnsolutions/qrscanner/DEtails.kt
@@ -0,0 +1,11 @@
+package com.gbnsolutions.qrscanner
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DEtails : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_details)
+    }
+}
